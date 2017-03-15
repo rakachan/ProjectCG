a953aa35dfe45fdfd59050c431c6b77f7a69cbc4
Pierre-Alexandre Lee <pierrealex.ly@gmail.com>
2017-03-15 22:01:11 +0100

    first scratch of bonus

--- hw2_trackball/grid/grid_vshader.glsl
+++ hw2_trackball/grid/grid_vshader.glsl
@@ -8,6 +8,7 @@ uniform mat4 MVP;
 uniform float time;
 
 void main() {
+#if
     uv = (position + vec2(1.0, 1.0)) * 0.5;
 
     // convert the 2D position into 3D positions that all lay in a horizontal
@@ -15,6 +16,12 @@ void main() {
     // TODO 6: animate the height of the grid points as a sine function of the
     // 'time' and the position ('uv') within the grid.
     float height = 0.1*sin(time+(uv.x+uv.y)*10);
+
+#else
+    uv = position;
+    float radius = sqrt(uv.x*uv.x + uv.y*uv.y);
+    float height = cos(time) * 0.5 * cos(14*radius - 4 * time) / (1 + 42*radius);
+#endif
     vec3 pos_3d = vec3(position.x, height, -position.y);
 
     gl_Position = MVP * vec4(pos_3d, 1.0);
