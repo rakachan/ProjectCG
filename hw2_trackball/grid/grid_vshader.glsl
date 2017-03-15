0a53194a6f8b02d1dbc5bf8b73be0e0465ce161c
Thomas Garcia <thomas.garcia@epfl.ch>
2017-03-15 22:15:05 +0100

    corrected the gris

--- hw2_trackball/grid/grid_vshader.glsl
+++ hw2_trackball/grid/grid_vshader.glsl
@@ -8,18 +8,15 @@ uniform mat4 MVP;
 uniform float time;
 
 void main() {
-#if 0
     uv = (position + vec2(1.0, 1.0)) * 0.5;
-
+#if 0
     // convert the 2D position into 3D positions that all lay in a horizontal
     // plane.
     // TODO 6: animate the height of the grid points as a sine function of the
     // 'time' and the position ('uv') within the grid.
     float height = 0.1*sin(time+(uv.x+uv.y)*10);
-
 #else
-    uv = position;
-    float radius = sqrt(uv.x*uv.x + uv.y*uv.y);
+    float radius = sqrt(position.x*position.x + position.y*position.y);
     float height = cos(time) * 0.5 * cos(14*radius - 4 * time) / (1 + 42*radius);
 #endif
     vec3 pos_3d = vec3(position.x, height, -position.y);
