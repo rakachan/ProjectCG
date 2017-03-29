34fc5216055c4987fbc7da5bab3c89d59210cc5b
Funky <gabriel.montauro@epfl.ch>
2017-03-29 14:25:39 +0200

    almost end of ex3

--- hw4p3_motionblur/screenquad/screenquad_fshader.glsl
+++ hw4p3_motionblur/screenquad/screenquad_fshader.glsl
@@ -5,17 +5,18 @@ uniform sampler2D velocityTex;
 out vec4 color;
 
 void main() {
-    int num_samples = 100;
-    float scale = 4;
-    vec2 velocity = texture(velocityTex, uv).xy * scale;
-    velocity = vec2(0.0,0.0);
-    vec4 Color = texture(colorTex, uv);
+    int num_samples = 200;
+    float scale = 600;
+    vec2 velocity = clamp(texture(velocityTex, uv).xy * scale, 0, 1);
+    //velocity = vec2(0.01,0.0);
+    vec3 Color = vec3(0,0,0);
 
-    for(int i = 1 ;i < num_samples ; ++i){
-        Color += texture(colorTex, uv - velocity*i/float(num_samples));
+    for(int i = 0 ;i < num_samples ; ++i){
+        Color += clamp(texture(colorTex, uv + (velocity*i/float(num_samples))).rgb, 0, 1);
     }
 
-    color = Color/float(num_samples);
+    Color /= float(num_samples);
+    color = vec4(Color,1);
     //velocity = vec2(0.001,0);
     //velocity = vec2(0.001,0.001);
     /// TODO: use the velocity vector stored in velocityTex to compute the line integral
