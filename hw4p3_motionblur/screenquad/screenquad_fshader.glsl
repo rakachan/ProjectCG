a5278dd8eccc02e51cd008d442258d37c9ea1d94
Funky <gabriel.montauro@epfl.ch>
2017-03-29 13:48:03 +0200

    clean version

--- hw4p3_motionblur/screenquad/screenquad_fshader.glsl
+++ hw4p3_motionblur/screenquad/screenquad_fshader.glsl
@@ -5,25 +5,20 @@ uniform sampler2D velocityTex;
 out vec4 color;
 
 void main() {
-    int num_samples = 70;
-    /// TODO: use the velocity vector stored in velocityTex to compute the line integral
-    vec2 velocity = texture(velocityTex, uv).xy * 10.0;
-    //velocity = vec2(0,0);
-    //I try debugging velocity here but it's hard to tell...
-    //velocity = vec2(0.001,0.001);
-    /**
-    if(velocity.x !=0 || velocity.y != 0){
-        color.y = 0.0;
-    }
-    **/
-    vec2 TexCoord = uv;
-    vec4 Color = texture(colorTex, TexCoord);
+    int num_samples = 100;
+    float scale = 4;
+    vec2 velocity = texture(velocityTex, uv).xy * scale;
+    velocity = vec2(0.0,0.0);
+    vec4 Color = texture(colorTex, uv);
 
-    for(int i = 1 ;i < num_samples ; ++i, TexCoord += velocity){
-        Color += texture(colorTex, TexCoord);
+    for(int i = 1 ;i < num_samples ; ++i){
+        Color += texture(colorTex, uv - velocity*i/float(num_samples));
     }
 
     color = Color/float(num_samples);
+    //velocity = vec2(0.001,0);
+    //velocity = vec2(0.001,0.001);
+    /// TODO: use the velocity vector stored in velocityTex to compute the line integral
     /// TODO: use a constant number of samples for integral (what happens if you take too few?)
     /// HINT: you can scale the velocity vector to make the motion blur effect more prominent
     /// HINT: to debug integration don't use the velocityTex, simply assume velocity is constant, e.g. vec2(1,0)
